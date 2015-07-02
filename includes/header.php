acc61a2c27ba81cf0e6750bb66b14a446fb78743
Nemanja Mandic <nemanjans84@gmail.com>
2015-07-02 18:23:42 +0200

    added navigation bar

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,16 @@
 			<section id="branding">
 				<a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="Logo"></a>
 			</section><!--END branding-->
+
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="venuetravel.php">Venue/Travel</a></li>
+					<li><a href="schedule.php">Schedule</a></li>
+					<li><a href="artists.php">Artists</a></li>
+					<li><a href="register.php">Register</a></li>
+				</ul><!--END nav navbar-nav-->
+			</nav><!--END navbar navbar-fixed-top"-->
 		</header><!--END clearfix-->
 	</div><!--END col-lg-12-->
 </div><!--END content row-->
