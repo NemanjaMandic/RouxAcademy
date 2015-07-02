ee5381e17062b75c0860bdc139a112dfb3ad4242
Nemanja Mandic <nemanjans84@gmail.com>
2015-07-02 19:02:18 +0200

    Added dropdown menu on artists

--- includes/header.php
+++ includes/header.php
@@ -5,12 +5,29 @@
 				<a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="Logo"></a>
 			</section><!--END branding-->
 
-			<nav class="navbar navbar-default navbar-fixed-top">
+			<nav class="navbar navbar-default">
 				<ul class="nav navbar-nav">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="venuetravel.php">Venue/Travel</a></li>
 					<li><a href="schedule.php">Schedule</a></li>
-					<li><a href="artists.php">Artists</a></li>
+					<li class="dropdown">
+					<a href="#" class="dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown">Artists<span class="caret"></span></a>
+					  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+					  	  <li><a tabindex="-1" href="artists.php">All Artists</a></li>
+					  	   <li class="divider"></li>
+					      <li><a tabindex="-1" href="artists.php#Barot_Bellingham">Barot Bellingham</a></li>
+					      <li><a tabindex="-1" href="artists.php#Gerard_Donahue">Gerard Donahue</a></li>
+					      <li><a tabindex="-1" href="artists.php#Jonathan_Ferrar">Jonathan Ferrar</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Lorenzo_Garcia">Lorenzo Garcia</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Hillary_Goldwynn">Hillary Goldynn</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Hassum_Harrod">Hassum Harrod</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Jennifer_Jerome">Jennifer Jerome</a></li>
+      					  <li><a tabindex="-1" href="artists.php#LaVonne_LaRue">LaVonne LaRue</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Riley_Rewington">Riley Rewington</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Constante_Smith">Constante Smith</a></li>
+      					  <li><a tabindex="-1" href="artists.php#Richard_Tweed">Richard Tweed</a></li>
+					  </ul><!--END dropdown-menu-->
+					 </li><!--END dropdown--> 
 					<li><a href="register.php">Register</a></li>
 				</ul><!--END nav navbar-nav-->
 			</nav><!--END navbar navbar-fixed-top"-->
